a00458c4377e6b115d7222fcc9081141bc2595f0
Pankaj Kumar <pankaj.gat@gmail.com>
2015-06-09 18:37:06 +0530

    Updated settings to show the status of typed-in location

--- src/iriemo/bangaloreweather/SettingsActivity.java
+++ src/iriemo/bangaloreweather/SettingsActivity.java
@@ -4,6 +4,7 @@ import sync.SunRiseSyncAdapter;
 import iriemo.bangaloreweather.data.WeatherContract;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -31,7 +32,7 @@ public class SettingsActivity extends AppCompatActivity
 	}
 	
 	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
 	{
@@ -51,7 +52,7 @@ public class SettingsActivity extends AppCompatActivity
 	/*
 	 * PrefernceFragment to support API level 11 onwards
 	 */
-		public static class PrefsFragment extends PreferenceFragment implements OnPreferenceChangeListener
+		public static class PrefsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener,Preference.OnPreferenceChangeListener
 		{
 			@Override
 			public void onCreate(Bundle savedInstanceState) 
@@ -64,9 +65,22 @@ public class SettingsActivity extends AppCompatActivity
 				bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
 				bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_temp_units_key)));
 			}
-			
-	
-			private void bindPreferenceSummaryToValue(Preference findPreference) 
+
+            @Override
+            public void onResume() {
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                sp.registerOnSharedPreferenceChangeListener(this);
+                super.onResume();
+            }
+
+            @Override
+            public void onPause() {
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                sp.unregisterOnSharedPreferenceChangeListener(this);
+                super.onPause();
+            }
+
+            private void bindPreferenceSummaryToValue(Preference findPreference)
 			{
 				findPreference.setOnPreferenceChangeListener(this);
 				
@@ -77,49 +91,52 @@ public class SettingsActivity extends AppCompatActivity
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object value) 
 			{
-				String stringValue = value.toString();
-				
-			if(preference.getKey().equals(getString(R.string.pref_location_key))) {
-				
-				/*FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-				weatherTask.execute(stringValue);*/
-                Utility.resetLocationStatus(getActivity());
-				SunRiseSyncAdapter.syncImmediately(getActivity());
-				
-			} else {
-				
-				getActivity().getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
-				
+                setPreferenceSummary(preference,value);
+                return  true;
 			}
-				if(preference instanceof ListPreference)
-				{
-					ListPreference listPreference = (ListPreference)preference;
-					
-					int prefIndex = listPreference.findIndexOfValue(stringValue);
-					
-					if(prefIndex >= 0)
-					{
-						preference.setSummary(listPreference.getEntries()[prefIndex]);
-					
-					}
-					else
-					{
-						preference.setSummary(stringValue);
-					}
-				}
-				if(preference instanceof EditTextPreference)
-				{
-					preference.setSummary(stringValue);
-				}
-				
-				if(preference instanceof CheckBoxPreference)
-				{
-					preference.setSummary(stringValue);
-				}
-				return true;
-			}
-	
-		}
+
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+                if(key.equals(getString(R.string.pref_location_key))){
+                    SunRiseSyncAdapter.syncImmediately(getActivity());
+                } else if(key.equals(getString(R.string.pref_temp_units_key))){
+                    getActivity().getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI,null);
+                }
+            }
+
+            private void setPreferenceSummary(Preference preference,Object value){
+                String stringValue = value.toString();
+                String key = preference.getKey();
+
+                if(preference instanceof  ListPreference){
+                    ListPreference listPreference = (ListPreference)preference;
+                    int prefIndex = listPreference.findIndexOfValue(stringValue);
+                    if(prefIndex >= 0){
+                        preference.setSummary(listPreference.getEntries()[prefIndex]);
+                    }
+                }else if(key.equals(getString(R.string.pref_location_key))){
+                    @SunRiseSyncAdapter.LocationStatus int status = Utility.getLocationStatus(getActivity());
+                    switch (status){
+                        case SunRiseSyncAdapter.LOCATION_STATUS_OK:
+                            preference.setSummary(stringValue);
+                            break;
+                        case SunRiseSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                            preference.setSummary(getString(R.string.pref_location_unknown_description,value.toString()));
+                            break;
+                        case SunRiseSyncAdapter.LOCATION_STATUS_INVALID:
+                            preference.setSummary(getString(R.string.pref_location_error_description,value.toString()));
+                            break;
+                        default:
+                            preference.setSummary(stringValue);
+                    }
+
+                }
+                else{
+                    preference.setSummary(stringValue);
+                }
+            }
+        }
 	
 	
 	
